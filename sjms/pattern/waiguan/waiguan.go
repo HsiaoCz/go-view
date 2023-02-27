afab61fed3629c484f201cd979eca2d6c7bfc591
HsiaoCz <984274788@qq.com>
2023-02-27 16:15:02 +0800

    simos

---
+++ sjms/pattern/waiguan/waiguan.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+// 外观模式
+type SubsystemA struct{}
+
+func (s *SubsystemA) MethodA() { fmt.Println("系统A的方法A") }
+
+type SubsystemB struct{}
+
+func (s *SubsystemB) MethodB() { fmt.Println("系统B的方法B") }
+
+type SubsystemC struct{}
+
+func (s *SubsystemC) MethodC() { fmt.Println("系统C的方法C") }
+
+// 提供一个外观模式
+// 它拥有这些类
+type WaiGuan struct {
+	systemA *SubsystemA
+	systenB *SubsystemB
+	systemC *SubsystemC
+}
+
+func (w *WaiGuan) MethodOne() {
+	w.systemA.MethodA()
+	w.systenB.MethodB()
+}
+func (w *WaiGuan) MethodTwo() {
+	w.systemA.MethodA()
+	w.systemC.MethodC()
+}
+
+func main() {
+	wg:=new(WaiGuan)
+	wg.MethodOne()
+	wg.MethodTwo()
+}
