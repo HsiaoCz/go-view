4528ffe903af748f5d5f6a6ceb0ac651adc91e5f
HsiaoCz <984274788@qq.com>
2023-02-28 16:32:05 +0800

    pattern

---
+++ sjms/pattern/strategy/main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+// 策略方法模式
+
+// 武器策略(抽象的策略)
+
+type WeaponStartegy interface {
+	UseWeapon()
+}
+
+// 具体的策略
+type Ak47 struct{}
+
+func (ak *Ak47) UseWeapon() { fmt.Println("使用AK47去战斗") }
+
+type Knife struct{}
+
+func (kn *Knife) UseWeapon() { fmt.Println("使用小刀去战斗") }
+
+// 环境类
+type Hero struct {
+	strategy WeaponStartegy // 拥有一个抽象的策略
+}
+
+// 设置一个策略的方法
+
+func (h *Hero) SetWeaponStrategy(s WeaponStartegy) {
+	h.strategy = s
+}
+
+// 战斗方法
+func (h *Hero) Fight() {
+	h.strategy.UseWeapon() // 调用具体的策略
+}
+func main() {
+	hero := Hero{}
+
+	// 更换策略
+	hero.SetWeaponStrategy(new(Ak47))
+	hero.Fight()
+
+	// 更换策略2
+	hero.SetWeaponStrategy(new(Knife))
+	hero.Fight()
+}
