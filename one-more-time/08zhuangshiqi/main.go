34daa9bd5027c05a3f0a917ebfa553ccdba5217f
HsiaoCz <984274788@qq.com>
2023-03-15 23:33:03 +0800

    one more time

---
+++ one-more-time/08zhuangshiqi/main.go
@@ -0,0 +1,62 @@
+package main
+
+import "fmt"
+
+// 装饰器模式
+
+// 抽象层
+type Phone interface {
+	Show()
+}
+
+// 抽象的装饰器类
+type Decorator struct {
+	phone Phone // 组合进来的Phone
+}
+
+func (d *Decorator) Show() {}
+
+// 实现类
+type HuaWei struct{}
+
+func (h *HuaWei) Show() { fmt.Println("秀出了华为手机") }
+
+type XiaoMi struct{}
+
+func (x *XiaoMi) Show() { fmt.Println("秀出了小米手机...") }
+
+// 具体的装饰器类
+
+type MoDectory struct {
+	Decorator //继承基础的装饰器类
+}
+
+func (md *MoDectory) Show() {
+	md.phone.Show() //调用被装饰者的构建
+	fmt.Println("贴膜的手机...")
+}
+
+func NewMoDecorator(phone Phone) Phone {
+	return &MoDectory{Decorator{phone: phone}}
+}
+
+type KeDectory struct {
+	Decorator // 继承自基础的类
+}
+
+func (kd *KeDectory) Show() {
+	kd.phone.Show() // 调用被装饰的构建
+	fmt.Println("带壳的手机")
+}
+
+func NewKeDecorator(phone Phone) Phone {
+	return &KeDectory{Decorator{phone: phone}}
+}
+
+func main() {
+	huawei := new(HuaWei)
+	huawei.Show()
+	// 装饰器，编程贴膜的华为手机
+	modectory := NewMoDecorator(huawei)
+	modectory.Show()
+}
