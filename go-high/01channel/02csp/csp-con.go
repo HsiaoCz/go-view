26398ad5fe2820ef5287badcb03d99fc5ff52b92
HsiaoCz <984274788@qq.com>
2023-04-18 21:57:11 +0800

    go-view

--- go-high/01channel/02csp/csp-con.go
+++ go-high/01channel/02csp/csp-con.go
@@ -1,3 +1,30 @@
 package main
 
+import "fmt"
+
 // csp
+type Task struct {
+	A int
+	B int
+}
+
+var ch = make(chan Task, 100)
+
+func Producer() {
+	for i := 0; i < 10; i++ {
+		ch <- Task{A: i + 3, B: i - 8}
+	}
+}
+
+func Consumer() {
+	for i := 0; i < 10; i++ {
+		task := <-ch
+		sum := task.A + task.B
+		fmt.Println(sum)
+	}
+}
+
+func main() {
+	go Producer() // 生产者协程
+	go Consumer() // 消费者协程
+}
